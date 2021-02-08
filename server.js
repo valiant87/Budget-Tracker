8588a7ff4d36bf9a54dffc8add5a2cff0a7347e7
Val Nimirenco <valnimirenco@gmail.com>
2021-02-07 20:14:33 -0800

    mongoose connection

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes

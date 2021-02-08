ecd598843af57da425af60c5181a341b761b05fa
Val Nimirenco <valnimirenco@gmail.com>
2021-02-07 20:40:26 -0800

    set useUnifiedTopology boolean to true

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });

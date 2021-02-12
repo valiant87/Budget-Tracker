4b212ed919166f15ab67097a5486a8b22e08233f
Val Nimirenco <valnimirenco@gmail.com>
2021-02-11 21:41:28 -0800

    changed cashed files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,21 @@
-// https://developers.google.com/web/fundamentals/primers/service-workers
-
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
-  "/db.js",
-  "/manifest.webmanifest",
   "/styles.css",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+  "/db.js",
 ];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-  );
-
-  // pre cache all static assets
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 
   self.skipWaiting();
@@ -44,33 +37,37 @@ self.addEventListener("activate", function (evt) {
 
   self.clients.claim();
 });
+
 // fetch
 self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
-    console.log("[Service Worker] Fetch (data)", evt.request.url);
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request)
-          .then((response) => {
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch((err) => {
-            console.log(err);
-            return cache.match(evt.request);
-          });
-      })
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response 200 store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
     );
+
     return;
   }
 
   evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      });
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
     })
   );
 });
